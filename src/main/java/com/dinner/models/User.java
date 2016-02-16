6dcfa9ca0f8f1dbf5b05d6f701cb0e9b142b244d
Sergii_Petrychenko <Sergii_Petrychenko@epam.com>
2016-02-16 21:01:36 +0200

    3 commit

--- src/main/java/com/dinner/models/User.java
+++ src/main/java/com/dinner/models/User.java
@@ -1,14 +1,33 @@
 package com.dinner.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Created by brut on 2/16/16.
  */
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @Column(name="login")
     private String login;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
     private String hashPassword;
 
-    public User(String login, String hashPassword) {
+    public User(){
+
+    }
+
+    public User(String login, String email, String hashPassword) {
         this.login = login;
+        this.email = email;
         this.hashPassword = hashPassword;
     }
 
@@ -18,6 +37,14 @@ public class User {
 
     public void setLogin(String login) {
         this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getHashPassword() {
